a027476ab0ddbf67b19f682d5bc2fbf1d184df8e
Avangardum <yurykuz65@gmail.com>
2026-09-12 13:23:30 +0300

    Refactor

--- Soteo.Core/Enums/Stat.cs
+++ Soteo.Core/Enums/Stat.cs
@@ -1,7 +1,6 @@
-using System.Collections.Immutable;
-
 namespace Soteo.Core.Enums;
 
+// TODO Use meters instead of pixels
 public enum Stat : byte
 {
     MaxHealth,
@@ -35,31 +34,4 @@ public enum Stat : byte
     /// Attack projectile speed in pixels per second
     /// </summary>
     AttackProjectileSpeed,
-}
-
-public static class StatExtensions
-{
-    private static readonly ImmutableList<Stat> _all;
-    private static readonly ImmutableList<Stat> _allResource;
-    private static readonly ImmutableList<Stat> _allComputed;
-    
-    static StatExtensions()
-    {
-        _all = Enum.GetValues<Stat>().ToImmutableList();
-        _allResource = _all.Where(it => it.IsResource).ToImmutableList();
-        _allComputed = _all.Where(it => !it.IsResource).ToImmutableList();
-    }
-    
-    extension (Stat self)
-    {
-        public static ImmutableList<Stat> All => _all;
-        public static ImmutableList<Stat> AllResource => _allResource;
-        public static ImmutableList<Stat> AllComputed => _allComputed;
-        
-        /// <summary>
-        /// Resource stats are independent values and can change freely for any reason.
-        /// Other stats are computed and cannot be changed directly.
-        /// </summary>
-        public bool IsResource => self is Stat.CurrentHealth or Stat.CurrentMana;
-    }
 }
---
+++ Soteo.Core/Extensions/StatExtensions.cs
@@ -0,0 +1,31 @@
+using System.Collections.Immutable;
+using Soteo.Core.Enums;
+
+namespace Soteo.Core.Extensions;
+
+public static class StatExtensions
+{
+    private static readonly ImmutableList<Stat> _all;
+    private static readonly ImmutableList<Stat> _allResource;
+    private static readonly ImmutableList<Stat> _allComputed;
+    
+    static StatExtensions()
+    {
+        _all = Enum.GetValues<Stat>().ToImmutableList();
+        _allResource = _all.Where(it => it.IsResource).ToImmutableList();
+        _allComputed = _all.Where(it => !it.IsResource).ToImmutableList();
+    }
+    
+    extension (Stat self)
+    {
+        public static ImmutableList<Stat> All => _all;
+        public static ImmutableList<Stat> AllResource => _allResource;
+        public static ImmutableList<Stat> AllComputed => _allComputed;
+        
+        /// <summary>
+        /// Resource stats are independent values and can change freely for any reason.
+        /// Other stats are computed and cannot be changed directly.
+        /// </summary>
+        public bool IsResource => self is Stat.CurrentHealth or Stat.CurrentMana;
+    }
+}
